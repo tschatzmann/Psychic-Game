7d3bd468c74d8b6290b17889e4b56ab791e878bd
tschatzmann <tarrta2002@gmail.com>
2018-12-15 17:00:59 -0500

    removed console.log statements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,33 @@
 // Creating variables to hold the number of wins, losses, and ties. They start at 0.
 // BUild variables
-var wins = 0;
-var losses = 0;
-var guessesRemaining = 6;
-var underScoreCount = 0;
-console.log('at set var underScoreCount ' + underScoreCount);
-var usedLetters = "";
-var LetterCount = 0;
-var alphbetArray = []
-var computerChoice = "";
+let wins = 0;
+let losses = 0;
+let guessesRemaining = 6;
+let underScoreCount = 0;
+let usedLetters = "";
+let LetterCount = 0;
+let alphbetArray = []
+let computerChoice = "";
 
 
-// Create variables that hold references to the places in the HTML where we want to display things.
-var directionsText = document.getElementById("directions-text");
-var userGuessText = document.getElementById("userGuess-text");
-var computerChoiceText = document.getElementById("computerChoice-text");
-var winsText = document.getElementById("wins-text");
-var lossesText = document.getElementById("losses-text");
-var usedLettersText = document.getElementById("usedLetters-text");
-var guessesRemainingtext = document.getElementById("guessesRemaining-text");
+// Create constiables that hold references to the places in the HTML where we want to display things.
+let directionsText = document.getElementById("directions-text");
+let userGuessText = document.getElementById("userGuess-text");
+let computerChoiceText = document.getElementById("computerChoice-text");
+let winsText = document.getElementById("wins-text");
+let lossesText = document.getElementById("losses-text");
+let usedLettersText = document.getElementById("usedLetters-text");
+let guessesRemainingtext = document.getElementById("guessesRemaining-text");
 
 //create an alphbet array
 for (i = 0; i < 26; i++) {
     alphbetArray.push((i + 10).toString(36));
 
 }
-console.log(alphbetArray)
-
 
 initvar();
 
-console.log(computerChoice);
-computerChoiceText.textContent = "The computer chose: " + computerChoice;
+// for testing computerChoiceText.textContent = "The computer chose: " + computerChoice;
 
 // This function is run whenever the user presses a key.
 document.onkeyup = function (event) {
@@ -39,10 +35,7 @@ document.onkeyup = function (event) {
     // Determines which key was pressed.
     var userGuess = event.key;
     //userGuess = userGuess.toLocaleLowerCase;
-    console.log("userGuess " + userGuess);
-    console.log('beginning at userGuess ' + 'guessesRemaining ' + guessesRemaining);
-
-
+ 
     // Hide the directions
     directionsText.textContent = "";
 
@@ -51,15 +44,12 @@ document.onkeyup = function (event) {
     }
     else {
         if (guessesRemaining > 0) {
-            console.log(userGuess + ' in guessesRemaining loop')
-            console.log('beginning if statement ' + 'guessesRemaining ' + guessesRemaining)
             if (userGuess == computerChoice) {
                 IsWin(guessesRemaining)
             } else {
                 WrongGuess(guessesRemaining);
             }
             usedLetters = usedLetters + " , " + userGuess;
-            console.log("usedLetters " + usedLetters);
         } else {
             IsLoss(guessesRemaining);
         }
@@ -77,7 +67,6 @@ document.onkeyup = function (event) {
 
 function initvar() {
     rando = Math.floor(Math.random() * 26 - 1);
-    console.log(rando);
     computerChoice = alphbetArray[rando];
     guessesRemaining = 6;
     usedLetters = "";
@@ -89,26 +78,20 @@ function initvar() {
     usedLettersText.textContent = "Letters Used: " + usedLetters;
     guessesRemainingtext.textContent = "Guesses Remaining: " + guessesRemaining;
     
-    console.log("in initvar function")
 }
 
 function IsFound() {
     position = computerChoice.indexOf(userGuess);
-    console.log('position of letter ' + position);
     return (position);
 }
 
 function IsUsedLetter(userGuess) {
-    console.log(usedLetters);
     return usedLetters.indexOf(userGuess);
 
 }
 
 function IsLoss() {
-    alert(" you lose");
     losses++;
-    console.log(losses);
-    console.log("in loss function " + underScoreCount);
     initvar();
 }
 
@@ -119,9 +102,7 @@ function WrongGuess() {
 
 
 function IsWin() {
-    console.log('in function is win ' + underScoreCount);
     wins++;
-    console.log('wins ' + wins);
     initvar();
 }
 
